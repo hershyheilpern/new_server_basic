a8f8ced022b03db14266b64b127c138d13bbdc74
hershyheilpern <hershyheilpern@gmail.com>
2024-11-07 18:37:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,10 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | 
 export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
 nvm install node
+nvm use
+
 
 apt install postgresql -y
+
+source ~/.bashrc 
+
