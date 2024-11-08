b969bf9e8d0e33e66563103a90493634d7a8069a
hershyheilpern <hershyheilpern@gmail.com>
2024-11-07 19:10:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt upgrade -y
-apt install git htop -y
+apt install git htop curl -y
 
 # wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
 # export NVM_DIR=$HOME/.nvm;
