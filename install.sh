01730da36b35a1e6addb1f253f6ed117b93377e6
hershyheilpern <hershyheilpern@gmail.com>
2024-11-10 17:11:18 -0500

    add gh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+### prepare for gh install
+mkdir -p -m 755 /etc/apt/keyrings
+wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+ 
 apt update
 apt upgrade -y
-apt install git htop curl -y
+apt install git htop curl gh postgresql -y
 
 # wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
 # export NVM_DIR=$HOME/.nvm;
@@ -24,8 +30,6 @@ node -v # should print `v22.11.0`
 
 # verifies the right npm version is in the environment
 npm -v # should print `10.9.0`
-
-apt install postgresql -y
 
 source ~/.bashrc 
 
